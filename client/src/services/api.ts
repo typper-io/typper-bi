5f92f7ccccb7789278aa8cc77b4680ce59c98cb0
João Barros <augustoj311@gmail.com>
2025-04-07 13:23:55 -0300

    Refactor API base URL configuration and update Docker Compose for Nginx proxy

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -1,22 +1,8 @@
 import axios, { AxiosError } from 'axios'
 import { signOut } from 'next-auth/react'
 
-const getBaseURL = () => {
-  if (process.env.NODE_ENV === 'development') {
-    return 'http://localhost:3001/backend'
-  }
-
-  const appDomain = process.env.APP_DOMAIN
-
-  if (!appDomain) {
-    return '/backend'
-  }
-
-  return `https://${appDomain}/backend`
-}
-
 export const api = axios.create({
-  baseURL: getBaseURL(),
+  baseURL: '/backend',
   transformRequest: axios.defaults.transformRequest,
   transformResponse: axios.defaults.transformResponse,
   withCredentials: true,
